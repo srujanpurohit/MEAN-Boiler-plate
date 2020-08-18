f58c870b7aa195d4e61274f3dc36f217507fabd3
Srujan Purohit <ideatoweb1@gmail.com>
2020-08-18 09:41:22 +0530

    1. added config loader for angular. 2. removed angular environment files 3. added environment files to gitignore 4. updated scripts to first load config then build 5. removed file replacement as now  environment.ts is generated dynamically

---
+++ src/environments/loadConfig.js
@@ -0,0 +1,44 @@
+const writeFile = require('fs').writeFileSync;
+
+const config = require('../../config.json');
+
+const targetPath = './src/environments/environment.ts';
+
+if (!config) {
+  throw new Error('Config file required');
+}
+if (!config.env) {
+  throw new Error('Env not defined');
+}
+if (!config[config.env]) {
+  throw new Error('No config defined for env: ' + config.env);
+}
+if (!config[config.env].frontend) {
+  throw new Error('No frontend config defined for env: ' + config.env);
+}
+if (!(typeof config[config.env].frontend.production === 'boolean')) {
+  throw new Error(
+    'Frontend config must contain production property with boolean value for env: ' +
+      config.env
+  );
+}
+if (
+  !(
+    config[config.env].frontend.baseUrl &&
+    typeof config[config.env].frontend.baseUrl === 'string'
+  )
+) {
+  throw new Error(
+    'Frontend config must contain baseUrl property with string value for env: ' +
+      config.env
+  );
+}
+
+const envConfigFile =
+  'export const environment = ' +
+  JSON.stringify({
+    ...config[config.env].frontend,
+    env: config.env
+  });
+
+writeFile(targetPath, envConfigFile, 'utf8');
